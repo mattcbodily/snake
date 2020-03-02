3a2e02b173f84d1472fe3d4839a9ed342aedf2bd
Matt Bodily <mattcbodily@gmail.com>
2020-03-02 14:26:35 -0700

    snake can eat fruit, working on tail

--- main.js
+++ main.js
@@ -8,9 +8,22 @@ let snake;
 
 (function setup(){
     snake = new Snake();
+    fruit = new Fruit();
+    fruit.spawn()
+
     window.setInterval(() => {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        fruit.draw();
         snake.draw();
         snake.move();
-      }, 250);
+
+        if(snake.eat(fruit)){
+            fruit.spawn();
+        }
+      }, 1000 / 6);
 }())
+
+window.addEventListener('keydown', ((evt) => {
+    const direction = evt.key.replace('Arrow', '');
+    snake.changeDirection(direction);
+}));
--- snake.js
+++ snake.js
@@ -3,14 +3,65 @@ function Snake(){
     this.y = 0;
     this.xSpeed = 10;
     this.ySpeed = 0;
+    this.total = 0;
+    this.tail = [];
 
     this.draw = function(){
         ctx.fillStyle = '#FFFFFF';
+        for (let i = 0; i < this.tail.length; i++) {
+            ctx.fillRect(this.tail[i].x,
+              this.tail[i].y, scale, scale);
+        }
+
         ctx.fillRect(this.x, this.y, scale, scale);
     }
 
     this.move = function(){
         this.x += this.xSpeed;
         this.y += this.ySpeed;
+
+        if (this.x > canvas.width) {
+            this.x = 0;
+          }
+      
+        if (this.y > canvas.height) {
+            this.y = 0;
+        }
+      
+        if (this.x < 0) {
+            this.x = canvas.width;
+        }
+      
+        if (this.y < 0) {
+            this.y = canvas.height;
+        }
+    }
+
+    this.changeDirection = function(direction){
+        switch(direction){
+            case 'Up':
+                this.xSpeed = 0;
+                this.ySpeed = -scale * 1;
+                break;
+            case 'Down':
+                this.xSpeed = 0;
+                this.ySpeed = scale * 1;
+                break;
+            case 'Left':
+                this.xSpeed = -scale * 1;
+                this.ySpeed = 0;
+                break;
+            case 'Right':
+                this.xSpeed = scale * 1;
+                this.ySpeed = 0;
+                break;
+        }
+    }
+
+    this.eat = function(fruit){
+        if(this.x === fruit.x && this.y === fruit.y){
+            return true;
+        }
+        return false;
     }
 }
